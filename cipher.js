bb15b24c5c0dcad06786602fe322ac07df3ac841
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-01 15:54:36 -0400

    hice otro cambio en js, pero sigue sin funcionar :(

--- cipher.js
+++ cipher.js
@@ -6,13 +6,16 @@ window.cipher = {
    
    for ( let i=0; i<mensajeIngresado.length;i++){
           
-         let transformacion = mensajeIngresado.charCodeAt(i);
+          let transformacion = mensajeIngresado.charCodeAt(i);
 
-            if (mensajeIngresado >=65 && mensajeIngresado <=90) {
-            codificado += String.fromCharCode((transformacion -65+key) % 26+65);
+            if (mensajeIngresado.charAt(i)>=65 && mensajeIngresado.charAt(i) <=90) {
+            codificado += String.fromCharCode((transformacion -65+desplazamiento) % 26+65);
      
-          } else if (mensajeIngresado >=97 && mensajeIngresado <=122) {
-            codificado += String.fromCharCode((transformacion -97+key) % 26+97);
+          } else if (mensajeIngresado.charAt(i)>=97 && mensajeIngresado.charAt(i)<=122) {
+            codificado += String.fromCharCode((transformacion -97+desplazamiento) % 26+97);
+
+          }else {
+	        	codificado += mensajeIngresado.charAt(i);
        
            
           } return codificado;
