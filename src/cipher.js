8aadc8e80a9ec66bd597c0d6493d0a12d124d7ae
Carolina Torres Duran <ca.torresduran@gmail.com>
2018-06-14 05:23:48 -0400

    ahora pasa 4 test, encode solucionado

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,18 @@
 window.cipher = {
   encode: (mensajeIngresado,desplazamiento) => {
 
-   let codificado=" ";
+   let codificado="";
    
-   for ( let i=0; i<mensajeIngresado.length;++i){ //bucle
+   for ( let i=0; i<mensajeIngresado.length;i++){ //bucle
           
           let transformacion = mensajeIngresado.charCodeAt(i); //convierte mensaje en ascii
 
             if (transformacion >= 65 && transformacion <= 90) { 
-            codificado += String.fromCharCode((transformacion - 65 + desplazamiento) % 26+65); 
+            codificado += String.fromCharCode((transformacion -65 + desplazamiento) %26+65); 
            
           }else if(transformacion >= 97 && transformacion <= 122){
-            codificado += String.fromCharCode((transformacion - 97 + desplazamiento) % 26 + 97);
+            codificado += String.fromCharCode((transformacion -97 + desplazamiento) %26+97);
     
-     
           } else { codificado += String.fromCharCode(transformacion); // para otros caracteres
 
           } 
@@ -25,17 +24,17 @@ window.cipher = {
   decode: (mensajeCodificado,desplazamiento) => {
 
      
-    let codificado=" ";
+    let codificado="";
    
-    for ( let i=0; i<mensajeCodificado.length;++i){ //bucle
+    for ( let i=0; i<mensajeCodificado.length;i++){ //bucle
            
            let transformacion2 = mensajeCodificado.charCodeAt(i); //convierte mensaje en ascii
  
              if (transformacion2 >= 65 && transformacion2 <= 90) { 
-             codificado += String.fromCharCode((transformacion2 - 65 - desplazamiento) % 26+65); 
+             codificado += String.fromCharCode((transformacion2 -65 - desplazamiento) %26+65); 
             
            }else if(transformacion2 >= 97 && transformacion2 <= 122){
-             codificado += String.fromCharCode((transformacion2 - 97 - desplazamiento) % 26 + 97);
+             codificado += String.fromCharCode((transformacion2 -97 - desplazamiento) %26+97);
      
       
            } else { codificado += String.fromCharCode(transformacion2); // para otros caracteres
